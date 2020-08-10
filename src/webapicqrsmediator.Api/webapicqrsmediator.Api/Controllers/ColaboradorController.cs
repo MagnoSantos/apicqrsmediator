33815ffba4221e6e5bc22fd09a4244925373a907
Magno Juliano Gonçalves Santos <santos.magno@mrv.com.br>
2020-08-10 16:12:34 -0300

    Cria controller para colaborador

--- src/webapicqrsmediator.Api/webapicqrsmediator.Api/Controllers/ColaboradorController.cs
+++ src/webapicqrsmediator.Api/webapicqrsmediator.Api/Controllers/ColaboradorController.cs
@@ -1,5 +1,11 @@
 ﻿
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using webapicqrsmediator.Domain;
 
 namespace webapicqrsmediator.Api.Controllers
 {
@@ -7,6 +13,22 @@ namespace webapicqrsmediator.Api.Controllers
     [Route("api/v{version:apiVersion}/[controller]")]
     public class ColaboradorController : ControllerBase
     {
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Adicionar(
+            [FromServices] IMediator mediator,
+            [FromBody] ColaboradorRequest command
+        )
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
 
+            var colaboradorAdicionado = await mediator.Send(command);
+
+            return Created(new Uri(Request.GetEncodedUrl()), colaboradorAdicionado);
+        }
     }
 }
